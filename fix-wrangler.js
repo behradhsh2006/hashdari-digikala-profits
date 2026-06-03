95e3036ba38bf9a228ac19021d6613d99e4f3a8d
behradhsh2006 <behrad.hsh@gmail.com>
2026-06-03 13:08:20 +0330

    Create fix-wrangler.js

---
+++ fix-wrangler.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = '.output/server/wrangler.json'
+
+if (fs.existsSync(path)) {
+  const config = JSON.parse(fs.readFileSync(path, 'utf8'))
+  if (config.assets?.binding === 'ASSETS') {
+    config.assets.binding = 'STATIC_ASSETS'
+    fs.writeFileSync(path, JSON.stringify(config, null, 2))
+    console.log('✓ Fixed ASSETS binding')
+  }
+}
